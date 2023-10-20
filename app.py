855c1793ac9148873d528c548a1969eebbab4cc8
David Sykes <david.sykes@octoenergy.com>
2023-10-20 18:51:16 +0100

    Add profile plot

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import streamlit as st
 import duckdb
+import plotly.express as px
 
 ## Load up database
 con = duckdb.connect()
-data_path = '../readings.parquet'
+data_path = 'sample.parquet'
 print("Loading data")
 con.sql(f"CREATE TABLE readings as SELECT * FROM '{data_path}';")
 
 ## App
-st.title('Test app')
+st.title('London Smart Meter Profiler')
 ids = con.sql("SELECT DISTINCT(LCLid) as ids FROM readings;").df()
 selected_id = st.selectbox("select id", ids['ids'])
-df = con.sql(f"SELECT * FROM readings WHERE LCLid = {selected_id};").df()
-st.dataframe(df)
+
+
+## Plot avg profile
+
+df = con.sql(f"SELECT hour(DateTime) as hour, avg(reading_value) as reading FROM readings WHERE LCLid = '{selected_id}' GROUP BY hour(DateTime);").df()
+st.plotly_chart(px.line(df, x='hour', y='reading'))
