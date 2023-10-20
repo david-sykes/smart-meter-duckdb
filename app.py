f86a162d1d481ef2fde66604f72bdd77b2cde046
David Sykes <david.sykes@octoenergy.com>
2023-10-20 18:14:01 +0100

    Test df

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import duckdb
 
 ## Load up database
 con = duckdb.connect()
+data_path = '../readings.parquet'
+print("Loading data")
+con.sql(f"CREATE TABLE readings as SELECT * FROM "{data_path};")
+df = con.sql("SELECT * FROM readings LIMIT 10;").df()
 
+## App
 st.title('Test app')
-
+st.dataframe(df)
